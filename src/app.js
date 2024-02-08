1e6dfbf568e531e08c6c6abf6216be743228e822
Juan Alzate <139303318+juanchoalzate@users.noreply.github.com>
2024-02-08 01:20:28 +0000

    generadorDominios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my", "your", "his", "her", "their", "its"];
+  let adj = [
+    "great",
+    "big",
+    "small",
+    "amazing",
+    "wonderful",
+    "awesome",
+    "fantastic",
+    "beautiful"
+  ];
+  let noun = ["jogger", "racoon", "car", "house", "tree", "cat", "dog", "bird"];
+  let extensions = [
+    ".com",
+    ".net",
+    ".us",
+    ".io",
+    ".es",
+    ".org",
+    ".info",
+    ".biz",
+    ".co",
+    ".tv"
+  ];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let domainName = pronoun[i] + adj[j] + noun[k] + ".com";
+
+        console.log(domainName);
+      }
+    }
+  }
 };
